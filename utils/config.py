8b5c30d59b4f484e372164ed528f328bae8fa1c4
stevenlyf428 <739218900@qq.com>
2026-08-27 12:31:17 +0800

    fix: use AgentRouter control-plane domain

--- utils/config.py
+++ utils/config.py
@@ -96,7 +96,7 @@ class AppConfig:
 			),
 			'agentrouter': ProviderConfig(
 				name='agentrouter',
-				domain='https://ps.air-outer.com',
+				domain='https://agentrouter.org',
 				login_path='/login',
 				sign_in_path=None,  # 无需签到接口，查询用户信息时自动完成签到
 				user_info_path='/api/user/self',
